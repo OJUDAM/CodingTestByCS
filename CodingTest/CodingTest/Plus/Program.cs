d9b15bdfc938f3e2e31dfb71c7474cc1cd7370a2
오주담 <dhwnkea@naver.com>
2020-11-15 13:00:10 +0900

    A+B

---
+++ CodingTest/CodingTest/Plus/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] spiltStr = Console.ReadLine().Split(' '); 
+            
+            Console.WriteLine(int.Parse(spiltStr[0]) + int.Parse(spiltStr[1]));
+
+            Console.ReadLine();
+        }
+    }
+}
