044379003e203c4b419a53fadfcd6904d9303efb
오주담 <dhwnkea@naver.com>
2020-11-17 14:48:23 +0900

    CountOfWord

---
+++ CodingTest/CodingTest/CountOfWord/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodingTest.CountOfWord
+{
+    class Program
+    {
+        static int Main()
+        {
+            string str = Console.ReadLine();
+            if(str ==" " || str == "")
+            {
+                Console.WriteLine(0);
+                Console.ReadLine();
+                return 0;
+            }
+            Console.WriteLine(str);
+            string[] words = str.Trim().Split();
+
+            Console.WriteLine(words.Length);
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
